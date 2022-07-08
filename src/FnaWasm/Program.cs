3bd7cd703ce75fe5f4c1e128ee5d3cd34ebed433
Wattsy <wattsyart@protonmail.com>
2022-07-08 15:55:25 -0400

    add blue screen test game

---
+++ src/FnaWasm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FnaWasm
+{
+    static class Program
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetDllDirectory(string lpPathName);
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            // https://github.com/FNA-XNA/FNA/wiki/4:-FNA-and-Windows-API#64-bit-support
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                SetDllDirectory(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    Environment.Is64BitProcess ? "x64" : "x86"
+                ));
+            }
+
+            using (WasmGame g = new WasmGame())
+            {
+                g.Run();
+            }
+        }
+    }
+}
---
+++ src/FnaWasm/WasmGame.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FnaWasm
+{
+    class WasmGame : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+
+        public WasmGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
